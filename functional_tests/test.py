2feb7aeb114d03063be90a661b1d1c7ea20080f2
Fong Ngo <fong.ngo.fn@gmail.com>
2018-09-25 05:55:26 +0100

    FT. type a new weight value and display it afterwards

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from django.test import TestCase
+from selenium.webdriver.common.keys import Keys
 
 
 class WeightTrackerTest(TestCase):
@@ -10,4 +11,17 @@ class WeightTrackerTest(TestCase):
         self.browser.quit()
 
     def test_home_page(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Weight Tracker', self.browser.title)
+
+        input = self.browser.find_element_by_id('weight_entry')
+        input.send_keys('60')
+        input.send_keys(Keys.Keys.ENTER)
+
+        time.sleep(1)
+
+        ul = self.browser.find_element_by_id('ul')
+        weight_entries = table.find_element_by_class_name('li')
+        self.assertIn('60', weight_entries)
+
         self.fail('--- end of tests ---')
